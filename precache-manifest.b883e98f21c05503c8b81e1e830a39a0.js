c2e4ade2c349f2807cfd46bff57b8be64cb781c0
vs0din <39313554+vs0din@users.noreply.github.com>
2019-01-22 13:53:22 +0200

    'pages'

---
+++ precache-manifest.b883e98f21c05503c8b81e1e830a39a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8b9f5c280d7da9a86579",
+    "url": "/static/js/main.8b9f5c28.chunk.js"
+  },
+  {
+    "revision": "46f208759a014f7672a0",
+    "url": "/static/js/1.46f20875.chunk.js"
+  },
+  {
+    "revision": "8b9f5c280d7da9a86579",
+    "url": "/static/css/main.0c9bf879.chunk.css"
+  },
+  {
+    "revision": "1356a1d23c18f37050109f0891c20bad",
+    "url": "/index.html"
+  }
+];
