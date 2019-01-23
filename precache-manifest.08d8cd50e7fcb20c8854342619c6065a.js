5fa22e63a38dbcbf38d5249298386177beae9f37
vs0din <39313554+vs0din@users.noreply.github.com>
2019-01-23 14:01:27 +0200

    'cascadedelete'

--- precache-manifest.08d8cd50e7fcb20c8854342619c6065a.js
+++ precache-manifest.08d8cd50e7fcb20c8854342619c6065a.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "8021f43ea61c11e20c29",
-    "url": "/static/js/main.8021f43e.chunk.js"
+    "revision": "ff7e0bb763c36fd00b7f",
+    "url": "/static/js/main.ff7e0bb7.chunk.js"
   },
   {
     "revision": "9575714a07ba6160ebee",
     "url": "/static/js/1.9575714a.chunk.js"
   },
   {
-    "revision": "8021f43ea61c11e20c29",
+    "revision": "ff7e0bb763c36fd00b7f",
     "url": "/static/css/main.d11e8769.chunk.css"
   },
   {
-    "revision": "d56aa4c13d438e0782066f787dfb64be",
+    "revision": "3e3d2161fc2492773c75c990ffdf9cf7",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.816ada6293d19431237ac6b091f85c53.js"
+  "/precache-manifest.08d8cd50e7fcb20c8854342619c6065a.js"
 );
 
 workbox.clientsClaim();
